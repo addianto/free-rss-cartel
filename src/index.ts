d0ff97e5599cc9724f92adf6fa0a7582e971fd6b
Daya Adianto <addianto@users.noreply.github.com>
2026-07-16 08:43:22 +0700

    spike: Handle fetch RSS feed (#1)
    Signed-off-by: Daya Adianto <addianto@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,28 @@
-import { Elysia } from 'elysia';
+import { Elysia, t } from 'elysia';
 import { CloudflareAdapter } from 'elysia/adapter/cloudflare-worker';
+import { env } from 'cloudflare:workers';
 
 export default new Elysia({
 	adapter: CloudflareAdapter,
 })
-	.get('/', () => 'Hello World!')
+	.get(
+		'/',
+		async ({ query }) => {
+			const expectedAuthorizationKey = env.AUTHORIZATION_KEY;
+			const response = await fetch(query.url, {
+				method: 'GET',
+				headers: {
+					'User-Agent':
+						'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3',
+				},
+			});
+			const data = await response.text();
+			return data;
+		},
+		{
+			query: t.Object({
+				url: t.String(),
+			}),
+		},
+	)
 	.compile();
