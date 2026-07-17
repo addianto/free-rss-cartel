15e02507e1afb003ae841bb6d4e8a985c03b0b9c
Daya Adianto <addianto@users.noreply.github.com>
2026-07-17 08:12:03 +0700

    feat: Check against allowed hostnames (#1)
    Signed-off-by: Daya Adianto <addianto@users.noreply.github.com>

--- src/index.ts
+++ src/index.ts
@@ -12,6 +12,12 @@ const createUrl = (urlString: string): URL | null => {
 	}
 };
 
+const isAllowedHostname = (hostname: string): boolean => {
+	const allowedHostnames = env.ALLOWED_HOSTNAMES?.split(',').map((h) => h.trim()) || [];
+
+	return allowedHostnames.length === 0 ? true : allowedHostnames.includes(hostname);
+};
+
 export default new Elysia({
 	adapter: CloudflareAdapter,
 })
@@ -23,12 +29,16 @@ export default new Elysia({
 			const cleanUrl = query.quotedUrl.replace(/['"]+/g, '');
 			const url = createUrl(cleanUrl);
 
-			if (!url || url.protocol === 'https:') {
+			if (!url || url.protocol !== 'https:') {
 				return new Response('Invalid URL', { status: 400 });
 			}
 
 			if (givenAuthorizationKey !== expectedAuthorizationKey) {
-				return new Response('Forbidden', { status: 403 });
+				return new Response('Forbidden: Invalid authorization key', { status: 403 });
+			}
+
+			if (!isAllowedHostname(url.hostname)) {
+				return new Response('Forbidden: Hostname not allowed', { status: 403 });
 			}
 
 			const response = await fetch(cleanUrl, {
